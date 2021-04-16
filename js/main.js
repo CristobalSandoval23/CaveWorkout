68a8e8ecc5cf4b2021bb8b0ddd6a328bf2609d2d
Cristóbal Sandoval <61505785+CristobalSandoval23@users.noreply.github.com>
2021-04-16 18:09:33 -0400

    Diseño finalizado de la pagina web

---
+++ js/main.js
@@ -0,0 +1,13 @@
+((d) => {
+    const $container_about_us = d.querySelector(".reverse");
+
+    window.addEventListener("resize", (e) => {
+        if(window.innerWidth > 700){
+            $container_about_us.classList.add("row-reverse");
+            }
+            else{
+                $container_about_us.classList.remove("row-reverse")
+            }
+
+        });
+  })(document);

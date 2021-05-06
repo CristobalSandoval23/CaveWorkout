5c3098b1ed54364be5bc915a236f1f6ab0bbc5d4
Cristóbal Sandoval <61505785+CristobalSandoval23@users.noreply.github.com>
2021-05-06 14:59:45 -0400

    se mejoro el acordion

--- js/main.js
+++ js/main.js
@@ -1,12 +1,27 @@
 ((d) => {
-    const $accordion = d.getElementsByClassName('contentBx');
-  console.log($accordion.length)
+    const $accordion = d.getElementsByClassName('contentBx'),
+          $icon_rest = d.getElementsByClassName('icon-rest'),
+          $icon_add = d.getElementsByClassName('icon-add');
+      var $close = true;
+
   for (let i = 0; i < $accordion.length; i++) {
-    $accordion[i].addEventListener('click', function(){
-      this.classList.toggle('active')
-    })
-    
-  }
+      $accordion[i].addEventListener('click', function(){
+        this.classList.toggle('active');
+        if($close){
+          $icon_rest[i].style.display = 'block';
+          $icon_rest[i].style.transition = '1s';
+          $icon_add[i].style.display = 'none';
+          $icon_add[i].style.transition = '1s';
+          $close = false;
+        }else{
+          $icon_rest[i].style.display = 'none';
+          $icon_rest[i].style.transition = '1s';
+          $icon_add[i].style.display = 'block';
+          $icon_add[i].style.transition = '1s';
+          $close = true;
+        }
+        })
+      }
 
     // window.addEventListener("resize", (e) => {
     //     if(window.innerWidth > 700){

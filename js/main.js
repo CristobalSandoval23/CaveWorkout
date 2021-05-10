ab9f901c65f6bd1eb124e660cfc22491afd2c82b
Cristóbal Sandoval <61505785+CristobalSandoval23@users.noreply.github.com>
2021-05-10 15:46:55 -0400

    se agrego el cookies

--- js/main.js
+++ js/main.js
@@ -3,12 +3,58 @@
           $icon_rest = d.getElementsByClassName('icon-rest'),
           $icon_add = d.getElementsByClassName('icon-add'),
           $bar = d.querySelector('.bar'),
-          $close = [];
-    // let $scroll = window.body.scrollHeight;
+          $close = [],
+          $btn_close = document.querySelector('#close'),
+          $cont_cookies = document.querySelector('.cookies-box'),
+          $modal = document.querySelector('.modal'),
+          $ver_p = document.querySelector('#ver_pol'),
+          $c_modal = document.querySelector('#close_modal'),
+          $ac_cookie = document.querySelector('#aceptar_cookie'),
+          $ac_cookie2 = document.querySelector('#aceptar_modal');
+
+          
+        localStorage.cookies1 == 'true'? $cont_cookies.style.bottom = '-200px':"";
+        
+        function aceptarCookies() {
+            localStorage.cookies1 = 'true'
+            $cont_cookies.style.bottom = '-200px'
+        
+            let expire = new Date()
+            expire = new Date(expire.getTime() + 776000000)
+            document.cookies = 'cookies1=aceptada; expire='+expire
+        }
+        
+        $ac_cookie.addEventListener('click', () => {
+            aceptarCookies();
+        })
+        
+        $ac_cookie2.addEventListener('click', () => {
+            aceptarCookies();
+            $modal.style.visibility = 'hidden'
+            $modal.style.opacity = '0'
+        })
+        
+        $btn_close.addEventListener('click', () => {
+            $cont_cookies.style.bottom = '-200px'
+        })
+        
+        
+        $ver_p.addEventListener('click', () => {
+            $modal.style.visibility = 'visible'
+            $modal.style.opacity = '1'
+        })
+        
+        $c_modal.addEventListener('click', () => {
+            $modal.style.visibility = 'hidden'
+            $modal.style.opacity = '0'
+        })
+
+
       for (let i = 0; i < $accordion.length; i++) {
         verdadero = true;
         $close.push(verdadero);
       }
+
 
       for (let i = 0; i < $accordion.length; i++) {
         $accordion[i].addEventListener('click', function(){
@@ -28,7 +74,19 @@
         }
         })
       }
+      // localStorage.setItem('hola', 'Bob')
+      // localStorage.removeItem('hola')
+      // sessionStorage.setItem('name', 'Carlos')
+      // Cookie.set("ieat", "true", {expires: 2});
 
+      // var myCookie = Cookie.get("ieat");
+      // // var title = Cookie.get("ieat");
+
+      // if(myCookie){
+
+      // }
+      // // sessionStorage.removeItem('name', 'Carlos')
+      // document.cookie = 'name=Kyle; expires=' + new Date(2022,0,1).toUTCString()
     window.addEventListener("scroll", (e) => {
           // console.log( window.pageYOffset)
           // console.log( window.pageXOffset)

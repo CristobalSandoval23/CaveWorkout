8bdd098b06207acb668b54013c6aa92dc3e33f53
Cristóbal Sandoval <61505785+CristobalSandoval23@users.noreply.github.com>
2021-06-01 15:32:09 -0400

    mejora de la PWA

--- sw.js
+++ sw.js
@@ -1,89 +1,80 @@
-const CACHE_STATIC_NAME  = 'static-v2';
-const CACHE_DYNAMIC_NAME = 'dynamic-v2';
-const CACHE_INMUTABLE_NAME = 'inmutable-v2';
-
-const CACHE_DYNAMIC_LIMIT = 50;
+const STATIC_CACHE    = 'static-v4';
+const DYNAMIC_CACHE   = 'dynamic-v2';
+const INMUTABLE_CACHE = 'inmutable-v1';
 
 
-function limpiarCache( cacheName, numeroItems ) {
+function actualizaCacheDinamico( dynamicCache, req, res ) {
 
 
-    caches.open( cacheName )
-        .then( cache => {
+    if ( res.ok ) {
 
-            return cache.keys()
-                .then( keys => {
-                    
-                    if ( keys.length > numeroItems ) {
-                        cache.delete( keys[0] )
-                            .then( limpiarCache(cacheName, numeroItems) );
-                    }
-                });
+        return caches.open( dynamicCache ).then( cache => {
 
+            cache.put( req, res.clone() );
             
+            return res.clone();
+
         });
+
+    } else {
+        return res;
+    }
+
+
+
 }
 
+const APP_SHELL = [
+    // '/',
+    '/index.html',
+    '/css/style.css',
+    '/assets/Antes.jpg',
+    '/assets/cristobal.jpg',
+    '/assets/Foto1.jpg',
+    '/assets/Foto2.jpg',
+    '/assets/Foto3.jpg',
+    '/assets/Foto4.jpg',
+    '/assets/Foto5.jpg',
+    '/assets/logo.png',
+    '/assets/nicolas.jpg',
+    '/assets/training-1.png',
+    '/js/main.js',
+    '/manifest.json',
+    '/pages/offline.html'
+];
 
+const APP_SHELL_INMUTABLE = [
+    '/index.html'
+];
 
 
 self.addEventListener('install', e => {
-    // const cacheProm = caches.open(cache_static_name)
-    // .then(cache => {
-    //    return cache.addAll([
-    //     '/',
-    //     '/index.html',
-    //    ])
-    // })
 
-    const cacheProm = caches.open( CACHE_STATIC_NAME )
-        .then( cache => {
 
-            return cache.addAll([
-                // '/',
-                '/index.html',
-                '/css/style.css',
-                '/assets/Antes.jpg',
-                '/assets/cristobal.jpg',
-                '/assets/Foto1.jpg',
-                '/assets/Foto2.jpg',
-                '/assets/Foto3.jpg',
-                '/assets/Foto4.jpg',
-                '/assets/Foto5.jpg',
-                '/assets/logo.png',
-                '/assets/nicolas.jpg',
-                '/assets/training-1.png',
-                '/js/main.js',
-                '/manifest.json',
-                '/pages/offline.html',
-            ]);
+    const cacheStatic = caches.open( STATIC_CACHE ).then(cache => 
+        cache.addAll( APP_SHELL ));
 
-        
-        });
+    const cacheInmutable = caches.open( INMUTABLE_CACHE ).then(cache => 
+        cache.addAll( APP_SHELL_INMUTABLE ));
 
-    const cacheInmutable = caches.open( CACHE_INMUTABLE_NAME )
-            .then( cache => cache.add('/index.html'));
-            console.log(cacheProm);
-    e.waitUntil( Promise.all([cacheProm, cacheInmutable]) );
+
+
+    e.waitUntil( Promise.all([ cacheStatic, cacheInmutable ])  );
 
 });
 
 
 self.addEventListener('activate', e => {
 
-
     const respuesta = caches.keys().then( keys => {
 
         keys.forEach( key => {
 
-            // static-v4
-            if (  key !== CACHE_STATIC_NAME && key.includes('static') ) {
+            if (  key !== STATIC_CACHE && key.includes('static') ) {
                 return caches.delete(key);
             }
-            if (  key !== CACHE_DYNAMIC_NAME && key.includes('dynamic') ) {
-                return caches.delete(key);
-            }
-            if (  key !== CACHE_INMUTABLE_NAME && key.includes('inmutable') ) {
+
+            if (  key !== DYNAMIC_CACHE && key.includes('dynamic') ) {
                 return caches.delete(key);
             }
 
@@ -98,44 +89,29 @@ self.addEventListener('activate', e => {
 
 
 
+self.addEventListener( 'fetch', e => {
 
-self.addEventListener('fetch', e => {
 
-    // 2- Cache with Network Fallback
-    const respuesta = caches.match( e.request )
-        .then( res => {
+    const respuesta = caches.match( e.request ).then( res => {
 
-            if ( res ) return res;
+        if ( res ) {
+            return res;
+        } else {
 
-            // No existe el archivo
+            return fetch( e.request ).then( newRes => {
 
-            return fetch( e.request ).then( newResp => {
+                return actualizaCacheDinamico( DYNAMIC_CACHE, e.request, newRes );
 
-                caches.open( CACHE_DYNAMIC_NAME )
-                    .then( cache => {
-                        cache.put( e.request, newResp );
-                        limpiarCache( CACHE_DYNAMIC_NAME, 50 );
-                    });
-
-                return newResp.clone();
-            })
-            .catch( err => {
-
-                if ( e.request.headers.get('accept').includes('text/html') ) {
-                    return caches.match('/pages/offline.html');
-                }
-
-            
             });
 
+        }
 
-        });
-
+    });
 
 
 
     e.respondWith( respuesta );
 
-
-
 });
+
+
